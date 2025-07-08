74d8bf8200665614cd12623bb15ddf6011695aa9
David Iacoviello <diacovmusic@gmail.com>
2025-07-08 19:50:13 -0400

    Update quickdrawLessCrossLinks.user.js

--- myVersion/quickdrawLessCrossLinks.user.js
+++ myVersion/quickdrawLessCrossLinks.user.js
@@ -3,8 +3,8 @@
 // @name           Quick Draw Links MIN CROSS
 // @category       Layer
 // @version        0.0.9.20210724.002500
-// @updateURL      https://raw.githubusercontent.com/IngressMyPlugins/main/myVersion/quickdrawLessCrossLinks.user.js
-// @downloadURL    https://raw.githubusercontent.com/IngressMyPlugins/main/myVersion/quickdrawLessCrossLinks.user.js
+// @updateURL      https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/quickdrawLessCrossLinks.user.js
+// @downloadURL    https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/quickdrawLessCrossLinks.user.js
 // @description    [diabloenmusica-0.0.9.20210724.002500] Quickly draw and move links from portal to portal on the map. Show crosslinks, for links on the map, as well as for drawn links. Store/Restore your projects. Added great circle support. Added fields layer. Export list of used portals with link count. Integrated Spectrum Colorpicker 1.8.1
 // @id             quick-draw-links@DiabloEnMusica
 // @namespace      https://softspot.nl/ingress/

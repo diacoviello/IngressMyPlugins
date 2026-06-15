65909a47d487a010f9adbafd0b4b724c372e3ecc
David Iacoviello <david.iacoviello@accesso.com>
2026-06-15 10:31:37 -0400

    fix SVG for streetview

--- myVersion/Diablo_bookmarks-addon.user.js
+++ myVersion/Diablo_bookmarks-addon.user.js
@@ -3,8 +3,8 @@
 // @name           Bookmarks add-on (fix)
 // @category       Diablo
 // @version        2.1.0.20260227.102800
-// @updateURL      https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/DanielOnDiordna/bookmarks-addon.meta.js
-// @downloadURL    https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/DanielOnDiordna/bookmarks-addon.user.js
+// @updateURL     https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/Diablo_bookmarks-addon.user.js
+// @downloadURL    https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/Diablo_bookmarks-addon.user.js
 // @description    [danielondiordna-2.1.0.20240227.204800] Bookmark plugin add-on, to replace the default yellow marker by a color marker (color change requires colorpicker or drawtools), and show bookmark names (layer), including optional scaling. Modified export file with timestamp in text/plain format. Also an option for bookmarks export to kml file format (for google maps). Add/remove bookmarks with filters for level, faction, captured, visited and resonator counts. Integrated Spectrum Colorpicker 1.8.1
 // @id             bookmarks-addon@DanielOnDiordna
 // @namespace      https://softspot.nl/ingress/

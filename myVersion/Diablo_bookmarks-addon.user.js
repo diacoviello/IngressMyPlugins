c6021e107960f98220f661b35f0c27de1233c4d9
David Iacoviello <david.iacoviello@accesso.com>
2026-06-15 11:45:36 -0400

    fixing console errors

--- myVersion/Diablo_bookmarks-addon.user.js
+++ myVersion/Diablo_bookmarks-addon.user.js
@@ -2,7 +2,7 @@
 // @author         DanielOnDiordna + DiabloEnMusica
 // @name           Bookmarks add-on (fix)
 // @category       Diablo
-// @version        2.1.0.20260227.102800
+// @version        2.1.0.20260615.120000
 // @updateURL      https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/Diablo_bookmarks-addon.user.js
 // @downloadURL    https://raw.githubusercontent.com/diacoviello/IngressMyPlugins/main/myVersion/Diablo_bookmarks-addon.user.js
 // @description    [danielondiordna-2.1.0.20240227.204800] Bookmark plugin add-on, to replace the default yellow marker by a color marker (color change requires colorpicker or drawtools), and show bookmark names (layer), including optional scaling. Modified export file with timestamp in text/plain format. Also an option for bookmarks export to kml file format (for google maps). Add/remove bookmarks with filters for level, faction, captured, visited and resonator counts. Integrated Spectrum Colorpicker 1.8.1

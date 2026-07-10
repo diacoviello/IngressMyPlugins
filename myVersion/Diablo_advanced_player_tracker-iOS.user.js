db0c7bed9675c62b8ddf01c7f42b89a62f004bd7
David Iacoviello <david.iacoviello@accesso.com>
2026-07-10 18:45:26 -0400

    fixes

--- myVersion/Diablo_advanced_player_tracker-iOS.user.js
+++ myVersion/Diablo_advanced_player_tracker-iOS.user.js
@@ -241,14 +241,18 @@ function wrapper( plugin_info ) {
 	}
 }
 
-const script=document.createElement( 'script' );
-var info={};
-if ( typeof GM_info!=='undefined'&&GM_info&&GM_info.script ) {
-	info.script={
-		version: GM_info.script.version,
-		name: GM_info.script.name,
-		description: GM_info.script.description
-	};
-}
-script.appendChild( document.createTextNode( '('+wrapper+')('+JSON.stringify( info )+')' ) );
-( document.body||document.head||document.documentElement ).appendChild( script );
+( function()
+{
+	var script=document.createElement( 'script' );
+	var info={};
+	if ( typeof GM_info!=='undefined'&&GM_info&&GM_info.script )
+	{
+		info.script={
+			version: GM_info.script.version,
+			name: GM_info.script.name,
+			description: GM_info.script.description
+		};
+	}
+	script.appendChild( document.createTextNode( '('+wrapper+')('+JSON.stringify( info )+')' ) );
+	( document.body||document.head||document.documentElement ).appendChild( script );
+} )();
